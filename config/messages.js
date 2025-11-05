bbabbec4997987190b3c007a45115aecc14b2425
Tyler <184896961+reallytyler@users.noreply.github.com>
2025-11-05 17:08:07 +0600

    Create messages.js

---
+++ config/messages.js
@@ -0,0 +1,50 @@
+module.exports = {
+    appearance: [
+        "🦀 A crab just scuttled into the server! Quick, catch it!",
+        "🦀 Look! A crab appeared! Don't let it get away!",
+        "🦀 Crab alert! A crab has been spotted in the wild!",
+        "🦀 Pinch, pinch! A crab is here! Catch it before it runs away!",
+        "🦀 Sideways walking friend appeared! Time to catch!",
+        "🦀 CRAB EMERGENCY! A crab needs catching!",
+        "🦀 Oh snap! A crab just showed up!",
+        "🦀 Beep boop! Crab detected! Initiate catch sequence!",
+        "🦀 YOINK! A crab materialized! Get it!",
+        "🦀 A wild crab appeared! It looks confused!",
+        "🦀 Crab delivery! Fresh crab just arrived!",
+        "🦀 Emergency! We have a crab situation!",
+        "🦀 BING BONG! Crab in the channel!",
+        "🦀 Warning: Cute crab overload! Catch it!",
+        "🦀 A crab has entered the chat! Say hello!",
+        "🦀 Crab-o-clock! Time to catch!",
+        "🦀 We've been blessed by the crab gods!",
+        "🦀 A crab is doing the sideways shuffle!",
+        "🦀 Crab raid! Everyone grab your nets!",
+        "🦀 Look at this funky little guy! Catch him!"
+    ],
+    
+    catchMessages: [
+        "**{user}** caught the crab! What a pro! 🦀",
+        "**{user}** snagged that crab like a boss! 💪",
+        "**{user}** is the crab master! Perfect catch! 🎯",
+        "**{user}** caught it! That crab didn't stand a chance! 😎",
+        "**{user}** with the epic crab catch! Legendary! 🌟",
+        "**{user}** caught the crab! So much skill! 🏆",
+        "**{user}** got the crab! Absolutely crushed it! 💥",
+        "**{user}** caught it! That was smooth! ✨",
+        "**{user}** snagged the crab! Incredible reflexes! ⚡",
+        "**{user}** caught the crab! Pure talent! 🎭"
+    ],
+    
+    levelUp: [
+        "🎉 **LEVEL UP!** You're now level {level}!",
+        "🌟 **PROMOTION!** Welcome to level {level}!",
+        "⚡ **RANK UP!** You've reached level {level}!",
+        "🏆 **ACHIEVEMENT!** Level {level} unlocked!",
+        "✨ **UPGRADE!** You're now level {level}!",
+        "🎊 **ADVANCEMENT!** Level {level} achieved!",
+        "💫 **EVOLUTION!** Welcome to level {level}!",
+        "🔥 **BREAKTHROUGH!** You've hit level {level}!",
+        "🚀 **ASCENSION!** Level {level} reached!",
+        "💎 **PRESTIGE!** You're now level {level}!"
+    ]
+};
